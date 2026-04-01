b6bf308328af4a0f8b5e8512a14fa5857327188a
UENO, M. <eunosium-1128@live.jp>
2026-04-01 18:14:38 +0900

    Update build script to improve build process

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+set -exo pipefail
 
 # Use our compilers instead of clang/clang++
 sed -i.bak 's|set(CMAKE_C_COMPILER|set(CMAKE_C_COMPILER_BAK|g' CMakeLists.txt
 sed -i.bak 's|set(CMAKE_CXX_COMPILER|set(CMAKE_CXX_COMPILER_BAK|g' CMakeLists.txt
 
-mkdir build
-cd build
-cmake $CMAKE_ARGS \
+cmake -S . -B build \ 
+  ${CMAKE_ARGS} \
   -DFILE_CHECK_EXECUTABLE=$BUILD_PREFIX/libexec/llvm/FileCheck \
   -DCLANG_EXECUTABLE=$BUILD_PREFIX/bin/clang \
   -DCLANGPP_EXECUTABLE=$BUILD_PREFIX/bin/clang++ \
@@ -14,6 +14,7 @@ cmake $CMAKE_ARGS \
   -DLLVM_AS_EXECUTABLE=$BUILD_PREFIX/bin/llvm-as \
   -DARM_ENABLED=no \
   -DISPC_NO_DUMPS=ON \
-  .. 
-make -j${CPU_COUNT}
-make install
+  -DISPC_SLIM_BINARY=ON
+cmake --build build --parallel ${CPU_COUNT}
+cmake --install build
+
